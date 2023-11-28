e48253dddba8d4f0f9ce67e741ba2afe4219907a
Acheul <seoul605.21@gmail.com>
2023-11-28 21:51:10 +0900

    pg

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use axum::{
+  async_trait,
   Router,
   http::StatusCode,
   handler::HandlerWithoutStateExt,
+  routing::{get, post},
+  extract::{self, State},
+  response::IntoResponse
 };
 use tower_http::{
   trace::TraceLayer, 
@@ -9,13 +13,20 @@ use tower_http::{
 };
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
+use bb8::{Pool, PooledConnection};
+use bb8_postgres::PostgresConnectionManager;
+use tokio_postgres::NoTls;
+
 use clap::Parser;
 use std::net::{SocketAddr, IpAddr, Ipv6Addr};
 use std::str::FromStr;
+use std::time::Duration;
 
 
-
-
+/***
+ * static
+ * + db(postgres)
+ */
 
 #[derive(Parser, Debug)]
 #[command(name="axum-server")]
@@ -28,6 +39,9 @@ pub struct Cli {
 
   #[arg(long="static-dir", short='d', default_value="./assets")]
   pub static_dir: String,
+
+  #[arg(long="db", default_value="postgres://pg:ss@localhost/db")]
+  db: String
 }
 
 
@@ -38,9 +52,17 @@ async fn main() {
   
   // logging
   tracing_subscriber::registry()
-  .with(tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| "crate_server=debug".into()))
-  .with(tracing_subscriber::fmt::layer())
-  .init();
+    .with(tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| "crate_server=debug".into()))
+    .with(tracing_subscriber::fmt::layer())
+    .init();
+
+  // set up connection pool
+  let manager = PostgresConnectionManager::new_from_stringlike(cli.db.as_str(), NoTls).unwrap();
+  let pool = Pool::builder()
+    .max_size(5)
+    .connection_timeout(Duration::from_secs(3))
+    .build(manager).await.expect("can't connect to database");
+
 
   // serve dir
   async fn handle_404() -> (StatusCode, &'static str) {
@@ -52,10 +74,11 @@ async fn main() {
   let serve_dir = ServeDir::new(&cli.static_dir).not_found_service(service);
 
   let app = Router::new()
-    
+    .route("/db/read", get(read_db))
+    .route("/db/insert/:number", post(insert_db))
     .nest_service("/static", serve_dir.clone())
     .layer(TraceLayer::new_for_http())
-    ;
+    .with_state(pool);
 
   // serve
   let addr = SocketAddr::from((
@@ -75,4 +98,54 @@ async fn shutdown_signal() {
     .await
     .expect("expect tokio signal ctrl-c");
   println!("signal shutdown");
+}
+
+
+
+// DB
+
+type ConnectionPool = Pool<PostgresConnectionManager<NoTls>>;
+
+
+async fn insert_db(
+  State(pool): State<ConnectionPool>,
+  extract::Path(number): extract::Path<String>
+) -> impl IntoResponse {
+
+  let number: i32 = number.parse().unwrap();
+
+  let conn = pool.get().await.unwrap();
+
+  let exe = conn
+    .execute("INSERT INTO aa (a) VALUES ($1::INT)", &[&number])
+    .await
+    .map_err(|e| println!("{:?}", e));
+
+  let res = if let Ok(exe) = exe {
+    format!("{:?}", exe)
+  } else {
+    "Err".to_string()
+  };
+  res
+}
+
+async fn read_db(
+  State(pool): State<ConnectionPool>,
+) -> impl IntoResponse {
+  let conn = pool.get().await;
+
+  let conn = pool.get().await.unwrap();
+
+  let rows = conn
+    .query("SELECT * FROM aa", &[])
+    .await
+    .map_err(|e| println!("{:?}", e));
+
+  let res: Vec<i32> = if let Ok(rows) = rows {
+
+    rows.iter().filter_map(|x| x.get(0)).collect()
+  } else {
+    vec![]
+  };
+  format!("{:?}", res)
 }
